f9ae99e0581aa074f2208d80b390baf51936a7d1
soniaml <sonialowe@protonmail.com>
2018-12-15 09:49:49 -0600

    Add the student controller

---
+++ AngularPeopleSearch/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularPeopleSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AngularPeopleSearch.Controllers
+{
+    //[Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        PersonDataAccessLayer personDataAccessLayer = new PersonDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Person/Index")]
+        public IEnumerable<Person> Index()
+        {
+            return personDataAccessLayer.GetAllPersons();
+        }
+
+        [HttpPost]
+        [Route("api/Person/Create")]
+        public int Create([FromBody] Person person)
+        {
+            return personDataAccessLayer.AddPerson(person);
+        }
+
+        [HttpGet]
+        [Route("api/Person/Edit")]
+        public int Edit([FromBody]Person person)
+        {
+            return personDataAccessLayer.UpdatePerson(person);
+        }        
+    }
+}
