fb044cdb7ba6d7375b0dd2d03c7b0d0d4258923a
soniaml <sonialowe@protonmail.com>
2018-12-16 14:53:56 -0600

    Display a list of people
    Display the details of the selected person

---
+++ AngularPeopleSearch/ClientApp/src/app/mock-people.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PEOPLE = [
+    { id: 11, firstName: 'Mr. Nice' },
+    { id: 12, firstName: 'Narco' },
+    { id: 13, firstName: 'Bombasto' },
+    { id: 14, firstName: 'Celeritas' },
+    { id: 15, firstName: 'Magneta' },
+    { id: 16, firstName: 'RubberMan' },
+    { id: 17, firstName: 'Dynama' },
+    { id: 18, firstName: 'Dr IQ' },
+    { id: 19, firstName: 'Magma' },
+    { id: 20, firstName: 'Tornado' }
+];
+//# sourceMappingURL=mock-people.js.map
--- AngularPeopleSearch/ClientApp/src/app/people/people.component.ts
+++ AngularPeopleSearch/ClientApp/src/app/people/people.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PEOPLE } from '../mock-people';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html'
 })
 export class PeopleComponent implements OnInit {
+  people = PEOPLE;
+  selectedPerson: Person;
 
-  person = 'Sonia';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(person: Person): void {
+    this.selectedPerson = person;
+  }
 }
